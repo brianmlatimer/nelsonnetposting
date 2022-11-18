216ecaa215f9acaf24514aedc46235264490c350
Angelo Nelson <angelonelson@outlook.com>
2022-11-17 19:17:52 -0600

    fixed intent issue

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import discord
 import os
 from meme_functionality import meme
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents(messages=True, message_content=True)
+client = discord.Client(intents=intents)
 botkey = os.getenv("botkey")
 
 @client.event
@@ -11,10 +12,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    print('buh', message.content)
     if message.author == client.user:
         return
     await meme.roulette(message, message.content)
-    if message.content == "!bruh":
+    if message.content == "!shitpost":
         await meme.shitpost(message,"this is a test")
     if message.content == "!last":
         await meme.send_last_meme(message)
